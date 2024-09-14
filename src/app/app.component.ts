189d89fce982cf66f54ff1126fa7f8f44adffd4e
Esther Gonzalez <estherdelcarmengonzalez30@gmail.com>
2024-09-13 18:57:19 -0600

    Primer commit Bases Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basesAngular';
+
+  pelicula={
+    nombre:'Super Man',
+    fechaLanzamiento:new Date(),
+    precio:123.65
+  }
+
+  duplicarNumero(valor:number):number{
+    return valor*2;
+  }
 }
